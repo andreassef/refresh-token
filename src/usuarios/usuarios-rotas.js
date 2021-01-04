695139a369edb5b414f060a3c405782bbb7f4cbc
andreassef <andreassef02@gmail.com>
2021-01-04 13:27:22 -0400

    [UPDATE] - adicao de novos middlewares na listagem de usuários

--- src/usuarios/usuarios-rotas.js
+++ src/usuarios/usuarios-rotas.js
@@ -1,5 +1,6 @@
 const usuariosControlador = require('./usuarios-controlador');
 const middlewaresAutenticacao = require('./middlewares-autenticacao');
+const autorizacao = require('../middlewares/autorizacao')
 
 module.exports = (app) => {
 
@@ -19,7 +20,7 @@ module.exports = (app) => {
   app
     .route('/usuario')
     .post(usuariosControlador.adiciona)
-    .get(usuariosControlador.lista);
+    .get( [middlewaresAutenticacao.bearer, autorizacao('usuario', 'readAny')],usuariosControlador.lista);
 
   app
     .route('/usuario/:id')
