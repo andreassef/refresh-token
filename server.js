a830b6eb49eafb3baee329f9e625e4ad23884248
andreassef <andreassef02@gmail.com>
2021-01-05 23:13:08 -0400

    [ADD] - Agrupando tratamento de erros com uso do Express

--- server.js
+++ server.js
@@ -6,7 +6,34 @@ require('./database');
 require('./redis/blocklist-access-token');
 require('./redis/allowlist-refresh-token');
 
+const { InvalidArgumentError } = require('./src/erros');
+const jwt = require('jsonwebtoken');
+
 const routes = require('./rotas');
+
 routes(app);
 
-app.listen(port);
+app.use((erro, request, response, next) => {
+    let status = 500;
+    const body = {
+        message: erro.message
+    }
+
+    if (erro instanceof InvalidArgumentError) {
+        status = 400
+    }
+
+    if (erro instanceof jwt.JsonWebTokenError) {
+        status = 401
+    }
+
+    if (erro instanceof jwt.TokenExpiredError) {
+        status = 401
+        body.expiredAt = erro.expiredAt
+    }
+
+    response.status(status)
+    response.json(body)
+})
+
+app.listen(port, () => console.log(`API ON na porta ${port}`));
