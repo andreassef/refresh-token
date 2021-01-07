32dea71bb92d2c746c426b48491c713e0119f7e5
andreassef <andreassef02@gmail.com>
2021-01-06 23:34:13 -0400

    [UPDATE] - Adicao de novo argumento para a classe

--- src/posts/posts-controlador.js
+++ src/posts/posts-controlador.js
@@ -20,7 +20,7 @@ module.exports = {
   async lista(req, res) {
     try {
       let posts = await Post.lista();
-      const conversor = new ConversorPost('json')
+      const conversor = new ConversorPost('json', req.acesso.todos.permitido ? req.acesso.todos.atributos : req.acesso.apenasSeu.atributos) // como segundo parametro, verificamos se o acesso a todos os atributos é permitido, caso posivito vamos listar todos os atributos
       if (!req.isAuth) {
         posts = posts.map(post => {
           post.conteudo = post.conteudo.substr(0, 10) + '... Voce precisa assinar o blog para ler o restante do post' 
